d4c5b2969d1ae3ce95b1bc40744a88fe820ac527
Joey Patterson <joeypatterson177@gmail.com>
2023-01-02 01:01:02 -0500

    create forecast object; convert Kelvins to Fahrenheit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const searchbar = document.querySelector("input");
 const searchBtn = document.querySelector(".searchBtn");
+let forecast = {};
 
 searchBtn.addEventListener("click", () => {
   const info = searchbar.value;
@@ -27,9 +28,34 @@ function sendWeatherRequest(info) {
       // return response.json();
     })
     .then((response) => {
-      console.log(response);
+      defineVariables(response);
+    })
+    .then(() => {
+      convertKelvinToFahrenheit();
     })
     .catch(function (error) {
       alert(error);
     });
+}
+
+function defineVariables(response) {
+  forecast = {
+    weather: response.weather[0].main,
+    temperature: response.main.temp,
+    feelsLike: response.main.feels_like,
+    tempHigh: response.main.temp_max,
+    tempLow: response.main.temp_min,
+  };
+}
+
+function convertKelvinToFahrenheit() {
+  const tempFah = (forecast.temperature - 273.15) * (9 / 5) + 32;
+  forecast.temperature = Math.round((tempFah + Number.EPSILON) * 100) / 100;
+  const feelsLikeFah = (forecast.feelsLike - 273.15) * (9 / 5) + 32;
+  forecast.feelsLike = Math.round((feelsLikeFah + Number.EPSILON) * 100) / 100;
+  const tempHighFah = (forecast.tempHigh - 273.15) * (9 / 5) + 32;
+  forecast.tempHigh = Math.round((tempHighFah + Number.EPSILON) * 100) / 100;
+  const tempLowFah = (forecast.tempLow - 273.15) * (9 / 5) + 32;
+  forecast.tempLow = Math.round((tempLowFah + Number.EPSILON) * 100) / 100;
+  console.log(forecast);
 }
